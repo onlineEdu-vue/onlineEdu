8067797c5d64d1d0ba1a1b854a9440c7c319cf25
sunseekers <1390639401@qq.com>
2018-02-27 19:56:03 +0800

    vue-cli

---
+++ onlineEdu----vue/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import axios from 'axios'
+import App from './App'
+import router from './router'
+import './assets/css/style.css'
+import './assets/css/font.css'
+Vue.config.productionTip = false
+window.$bus= new Vue();
+Vue.prototype.$http=axios
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
